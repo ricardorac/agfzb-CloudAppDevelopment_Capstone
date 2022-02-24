8a1f6f0a6c26498cc79b40de0f662ade9630a6cc
rac <ricardo.rac@gmail.com>
2022-02-24 12:24:37 -0300

    Add registration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,21 +9,22 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    path(route='', view=views.get_dealerships, name='index'),
+
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration', views.registration_request, name='registration'),
 
     # path for login
     path(route='login', view=views.login_request, name='login'),
 
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-
-    path(route='', view=views.get_dealerships, name='index'),
-    path(route='about', view=views.about, name='about'),
-    path(route='contact', view=views.contact, name='contact'),
 
     # path for dealer reviews view
 
