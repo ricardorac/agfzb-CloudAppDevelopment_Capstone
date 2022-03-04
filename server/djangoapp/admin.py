cd481ffcbb6de92b137672c1da611baa91da38d0
rac <ricardo.rac@gmail.com>
2022-03-04 10:44:09 -0300

    Adding admin mappings

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
+
+from server.djangoapp.models import CarMake, CarModel
 # from .models import related models
 
 
 # Register your models here.
 
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['name', 'dealer_id', 'type', 'year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
