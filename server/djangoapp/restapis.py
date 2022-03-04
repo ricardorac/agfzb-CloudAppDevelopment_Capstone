d3f3318b612a53b885f066233b8542d02f45955c
rac <ricardo.rac@gmail.com>
2022-03-04 12:39:04 -0300

    create a get_dealer_by_id and get_dealers_by_state

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -39,6 +39,40 @@ def get_dealers_from_cf(url, **kwargs):
             results.append(dealer_obj)
 
     return results
+
+
+def get_dealers_by_state(url, state, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, state=state)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result
+        # For each dealer object
+        for dealer in dealers:
+            dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
+                                   id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
+                                   short_name=dealer["short_name"],
+                                   st=dealer["st"], zip=dealer["zip"], state=dealer["state"])
+            results.append(dealer_obj)
+
+    return results
+
+
+def get_dealer_by_id(url, dealerId, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, dealerId=dealerId)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealer = json_result
+        dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
+                                id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
+                                short_name=dealer["short_name"],
+                                st=dealer["st"], zip=dealer["zip"], state=dealer["state"])
+        results.append(dealer_obj)
+
+    return results
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
