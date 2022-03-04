69535033eb76beac384719516c4b0ffc4961fe06
rac <ricardo.rac@gmail.com>
2022-03-04 10:29:38 -0300

    Adding models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,58 @@
+from datetime import date
+from pydoc import describe
 from django.db import models
 from django.utils.timezone import now
-
+from dataclasses import dataclass
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100, default="Car Make")
+    description = models.CharField(null=True, max_length=255)
+
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    CHOICES = [("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon")]
+    name = models.CharField(null=False, max_length=100, default="Car Model")
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    type = models.CharField(null=True, max_length=10, choices = CHOICES, default="SUV")
+    year = models.DateField()
 
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Car Make: " + self.car_make + ", " + \
+               "Dealer Id: " + self.dealer_id + ", " + \
+               "Type: " + self.type + ", " + \
+               "Year: " + self.year
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+@dataclass
+class CarDealer:
+    id: int
+    city: str
+    state: str
+    st: str
+    address: str
+    zip: str
+    lat: float
+    long: float
+    short_name: str
+    full_name: str
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+@dataclass
+class DealerReview:
+    id: int
+    name: str
+    dealership: int
+    review:str
+    purchase: bool
+    purchase_date: date
+    car_make: str
+    car_model: str
+    car_year: date
