0a6b2ed3574f1d5a9d52794021f8314bfb610c57
rac <ricardo.rac@gmail.com>
2022-03-08 11:02:53 -0300

    Adding dealership Bootstrap table

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -90,12 +90,11 @@ def registration_request(request):
 def get_dealerships(request):
     if request.method == "GET":
         url = API_URL + "/get-dealers"
+        context = {}
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
-        # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
-        # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context["dealerships"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
